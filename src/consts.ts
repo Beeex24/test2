78bfe9f5e4816eae439bb7f81a8df1ec948d881e
Beeex24 <shaling113@gmail.com>
2025-06-26 21:16:00 +0900

    Add new feature: サインを追加

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = 'ANYGAMES放送局';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const SITE_HERO_IMAGE = '/path/to/00573.png';
+export const SITE_HERO_IMAGE = '/00573.png';
